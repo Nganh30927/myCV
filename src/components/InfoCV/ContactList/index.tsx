28944140404fbcf49f3c68150094b6f7433d51e9
Ngoc Anh <ngocanh30927@gmail.com>
2024-07-10 02:16:17 +0700

    up

--- src/components/InfoCV/ContactList/index.tsx
+++ src/components/InfoCV/ContactList/index.tsx
@@ -1,9 +1,8 @@
-import styles from "./ContactList.module.css";
+// import styles from "./ContactList.module.css";
 
 import { profiles } from "../../../data/Profiles";
 
 type SingleContactProp = {
-  icons?: React.ReactNode;
   desc: string;
 };
 
@@ -16,7 +15,6 @@ const SingleContact = ({ props }: { props: SingleContactProp }) => {
           className="flex items-center p-3 text-base font-normal text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
         >
           <span className="flex-1 ml-3 whitespace-nowrap">
-            {props.icons}
             {props.desc}
           </span>
         </a>
